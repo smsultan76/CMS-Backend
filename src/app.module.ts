531374a66339a92f8187582a2e1815971e556f34
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2025-10-29 16:04:11 +0600

    Created Category Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
 import configuration from './config/configuration';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     load: [configuration],
-  }), AuthModule, UsersModule],
+  }), AuthModule, UsersModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
