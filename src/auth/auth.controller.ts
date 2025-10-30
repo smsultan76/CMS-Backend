a4c648acd3a67babc373b39cc890e254ed4bc7c5
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2025-10-30 20:28:31 +0600

    making clean

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,6 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 import { AuthResponseDto } from './dto/auth-response.dto';
-import { LocalAuthGuard } from './guards/local-auth.guard';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 import { Public } from '../common/decorators/public.decorator';
 
@@ -13,13 +12,11 @@ import { Public } from '../common/decorators/public.decorator';
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
-
   @Post('register')
   @ApiOperation({ summary: 'User Registration' })
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
   }
-
 
   @Public()
   @Post('login')
